cbd4bfd73be40997049a3bff74d454722f6eca78
Merijn <99054535@mydavinci.nl>
2021-06-11 12:00:37 +0200

    getting started with the right positions
    it isn't dry so it is bad

--- function.php
+++ function.php
@@ -10,4 +10,47 @@ require "dbConnect.php";
       $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     return $stmt->fetchAll();
     }
+
+    function getTasks() {
+      $connection = DbConnect();
+      $stmt = $connection->prepare("SELECT * FROM tasks");
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    return $stmt->fetchAll();
+    }
+
+    function getToDo() {
+      $connection = DbConnect();
+      $stmt = $connection->prepare("SELECT * FROM tasks WHERE list_id = 1");
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      return $stmt->fetchAll();
+
+    }
+
+    function getBusy() {
+      $connection = DbConnect();
+      $stmt = $connection->prepare("SELECT * FROM tasks WHERE list_id = 2");
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      return $stmt->fetchAll();
+
+    }
+
+    function getDone() {
+      $connection = DbConnect();
+      $stmt = $connection->prepare("SELECT * FROM tasks WHERE list_id = 3");
+      $stmt->execute();
+    
+      // set the resulting array to associative
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      return $stmt->fetchAll();
+
+    }
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 include "function.php";
 $lists = getLists();
-
+$todo = getToDo();
+$busy = getBusy();
+$done = getDone();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +19,10 @@ $lists = getLists();
    <?php 
    foreach($lists as $list) {
     echo "<h1>" . $list['name'] . "</h1>";
-
+        foreach($todo as $todo) {
+            echo "<p>" . $todo['name'] . "</p>";
+            echo "<p>" . $todo['description'] . "</p>";
+        }
    }
    ?>
 </body>
