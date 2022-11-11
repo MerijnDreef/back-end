c183beec9f3b04180b060ca984848e1968788af8
Merijn <99054535@mydavinci.nl>
2022-11-11 11:00:47 +0100

    updated dbConnect to have the correct database password

--- dbConnect.php
+++ dbConnect.php
@@ -3,7 +3,7 @@ function DbConnect() {
   $servername = "localhost";
   $username = "root";
   $databasename = "back-end";
-  $password = "";
+  $password = "mysql";
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
     // set the PDO error mode to exception
